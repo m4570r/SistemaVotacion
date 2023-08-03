e91f355a5ff976c5d9d10ea0f883417125e2a832
Miguel Ángel <91635371+m4570r@users.noreply.github.com>
2023-08-03 09:40:26 -0400

    Update index.php
    Actualization error the commentary

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<!--
-    ----------------------------------------------------------
+<!------------------------------------------------------------
     Nombre del proyecto: Sistema de votacion electronica
     Descripción: Este proyecto es un sistema de votación electrónica que permite a los usuarios emitir su voto para un candidato determinado. Está construido con MySQL para la gestión de la base de datos, PHP para el procesamiento en el lado del servidor, HTML para la estructura de la página web, y JavaScript para la funcionalidad en el lado del cliente.
     Autor: Miguel Gonzalez
     Email: mgonzalez.gnu@gmail.com
-    ----------------------------------------------------------
--->
+    ------------------------------------------------------------>
     <title>Sistema de Votación</title>
     
     <link rel="icon" href="favicon.ico" type="image/x-icon">
